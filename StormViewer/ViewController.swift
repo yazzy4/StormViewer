e7f1d9fdb1017f9ebb2400012021cc308b27756f
yazzy4 <yazburrell@pursuit.org>
2020-07-07 14:52:22 -0400

    first commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Yaz Burrell on 7/7/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
