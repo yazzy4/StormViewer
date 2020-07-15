f57abf93d891fd0323b1074f22c4d2ef96691259
yazzy4 <yazburrell@pursuit.org>
2020-07-15 17:17:49 -0400

    added some minor tweaks to the UI

--- StormViewer/VCs/DetailViewController.swift
+++ StormViewer/VCs/DetailViewController.swift
@@ -16,10 +16,24 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = selectedImage
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
     
 
